c509dba0afb362f408ed53903f454a3478ee92a4
Harsh Kumar <harsh.kumar9118b@gmail.com>
2025-08-01 19:04:59 +0530

    multiply two numbers

---
+++ multiply_two_numbers.java
@@ -0,0 +1,8 @@
+public class multiply_two_numbers {
+    public static void main(String[] args) {
+        int a = 4;
+        int b = 8;
+        int product  = a * b;
+        System.out.println("The product of a and b is: " + product);
+    }
+}
