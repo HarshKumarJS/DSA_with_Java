496e0ff2c8366a0b86f45c1ff3d34a9eec6dbc50
Harsh Kumar <harsh.kumar9118b@gmail.com>
2025-08-16 22:07:24 +0530

    greater than equal to & less than equal to operator

---
+++ Operators/greaterequal_and_lessequal_operator.java
@@ -0,0 +1,19 @@
+public class greaterequal_and_lessequal_operator {
+    public static void main(String[] args) {
+
+        int a = 8;
+        int b = 3;
+        System.out.println(a>=b);  // It will return true
+        System.out.println(a<=b);  // It will return false
+
+        int c = 5;
+        int d = 5;
+        System.out.println(c>=d); // It will return true
+        System.out.println(c<=d); // It will return true
+
+        int e = 4;
+        int f = 9;
+        System.out.println(e>=f); // It will return false
+        System.out.println(e<=f); // It will return true
+    }
+}
