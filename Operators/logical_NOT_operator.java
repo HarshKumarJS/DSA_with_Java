0bccece85d4fb7be0b4c910fab215f532974c99c
Harsh Kumar <harsh.kumar9118b@gmail.com>
2025-08-16 22:30:19 +0530

    logical NOT operator

---
+++ Operators/logical_NOT_operator.java
@@ -0,0 +1,10 @@
+public class logical_NOT_operator {
+    public static void main(String[] args) {
+
+        int a = 4;
+        int b = 3;
+
+        System.out.println( !(a>b) );  // It will return false
+        System.out.println( !(a<b) );  // It will return true
+    }
+}
