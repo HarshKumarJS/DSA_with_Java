066f16d60acdaed26483463a488079e7c9a2e273
Harsh Kumar <harsh.kumar9118b@gmail.com>
2025-08-16 21:26:36 +0530

    not equal operator

---
+++ Operators/not_equal_operator.java
@@ -0,0 +1,12 @@
+public class not_equal_operator {
+    public static void main(String[] args) {
+
+        int a = 4;
+        int b = 4;
+        System.out.println(a!=b);  // It will return false
+
+        int c = 2;
+        int d = 3;
+        System.out.println(c!=d);  // It will return true
+    }
+}
