800ace8ba3d6fcb56dab03d75dab96f1e0852d5f
Harsh Kumar <harsh.kumar9118b@gmail.com>
2025-08-16 23:04:48 +0530

    largest of two numbers

---
+++ Conditional_statements/largest_of_two_numbers.java
@@ -0,0 +1,14 @@
+public class largest_of_two_numbers {
+    public static void main(String[] args) {
+
+        int x = 7;
+        int y = 3;
+
+        if(x>y){
+            System.out.println("x is the largest number");
+        }
+        else{
+            System.out.println("y is the largest number");
+        }
+    }
+}
