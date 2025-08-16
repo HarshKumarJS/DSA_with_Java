8ccffbcaa5b485f5bd723542a387abcc25787773
Harsh Kumar <harsh.kumar9118b@gmail.com>
2025-08-16 22:58:31 +0530

    simple if-else code

---
+++ Conditional_statements/if_else.java
@@ -0,0 +1,14 @@
+public class if_else {
+    public static void main(String[] args) {
+
+        int a = 5;
+        int b = 3;
+
+        if(a>b){
+            System.out.println("true");
+        }
+        else{
+            System.out.println("false");
+        }
+    }
+}
