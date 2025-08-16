a55e76734b37d4e773b9312aa500e9e1c76b30d5
Harsh Kumar <harsh.kumar9118b@gmail.com>
2025-08-16 21:55:54 +0530

    greater and lessthan operator

---
+++ Operators/greater_and_lessthan_operator.java
@@ -0,0 +1,11 @@
+public class greater_and_lessthan_operator {
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 3;
+
+        System.out.println(a<b);   // It will return false
+
+        System.out.println(a>b);   // It will return true
+    }
+}
