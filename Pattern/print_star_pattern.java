495eaf4041e872b23961519e161df950fb85c4b9
Harsh Kumar <harsh.kumar9118b@gmail.com>
2025-08-16 21:03:53 +0530

    print_star_pattern

---
+++ Pattern/print_star_pattern.java
@@ -0,0 +1,13 @@
+public class print_star_pattern {
+    public static void main(String[] args) {
+
+        //  Method 1
+        //System.out.println("****\n***\n**\n*");
+
+        //  Method 2
+        System.out.println("****");
+        System.out.println("***");
+        System.out.println("**");
+        System.out.println("*");
+    }
+}
