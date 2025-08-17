78b950e19b4d11123b74429e36188d6583b4fb8e
Harsh Kumar <harsh.kumar9118b@gmail.com>
2025-08-17 15:02:26 +0530

    the number is odd or even

---
+++ Conditional_statements/odd_or_even.java
@@ -0,0 +1,13 @@
+public class odd_or_even {
+    public static void main(String[] args) {
+
+        int a = 3156347;
+
+        if(a%2==0){
+            System.out.println("The number is even");
+        }
+        else {
+            System.out.println("The number is odd");
+        }
+    }
+}
