fff407e47f538ae2b2d2bc7eac7fa0fb71dce9a5
Harsh Kumar <harsh.kumar9118b@gmail.com>
2025-10-24 20:35:42 +0530

    correct code

--- Conditional_statements/switch_statement.java
+++ Conditional_statements/switch_statement.java
@@ -9,13 +9,13 @@ public class switch_statement {
         int number = sc.nextInt();
 
         switch (number) {
-            case 1: System.out.print("Samosa");
+            case 1: System.out.println("Samosa");
             break;
-            case 2: System.out.print("Burger");
+            case 2: System.out.println("Burger");
             break;
-            case 3: System.out.print("Pizza");
+            case 3: System.out.println("Pizza");
             break;
-            default : System.out.print("Momos");
+            default : System.out.println("Better luck next time");
         }
     }
 }
