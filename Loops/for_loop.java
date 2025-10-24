961b551d497009c823a50e873b62335efe3d5955
Harsh Kumar <harsh.kumar9118b@gmail.com>
2025-10-24 21:03:27 +0530

    added loops folder and for loop program

---
+++ Loops/for_loop.java
@@ -0,0 +1,13 @@
+// This program prints numbers from 1 to 10 using a for loop
+
+public class for_loop {
+    // The main method is the entry point of the program
+    public static void main(String[] args) {
+
+        // Loop starts from 1 and runs till 10 (inclusive)
+        for (int i = 1; i <= 10; i++) {
+            // Print the current value of i in each iteration
+            System.out.println(i);
+        }
+    }
+}
