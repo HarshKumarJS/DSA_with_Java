8c811cfffb4e94456786e72b25d1dc4fc12b5ed5
Harsh Kumar <harsh.kumar9118b@gmail.com>
2025-10-25 19:14:40 +0530

    while loop

---
+++ Loops/while_loop.java
@@ -0,0 +1,18 @@
+// This program prints numbers from 1 to 50 using a while loop
+
+public class while_loop {
+    public static void main(String[] args) {
+
+        // Initialize variable i with value 1
+        int i = 1;
+
+        // Continue looping as long as i is less than or equal to 50
+        while (i <= 50) {
+            // Print the current value of i
+            System.out.println(i);
+
+            // Increment i by 1 after each iteration
+            i++;
+        }
+    }
+}
