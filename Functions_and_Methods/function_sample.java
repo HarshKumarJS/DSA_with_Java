9582f53ab3adde4199a03f7b27edc07870b62ddb
Harsh Kumar <harsh.kumar9118b@gmail.com>
2025-10-29 07:36:47 +0530

    added a sample function file

---
+++ Functions_and_Methods/function_sample.java
@@ -0,0 +1,9 @@
+public class function_sample {
+    public static void main(String[] args) {
+        printpayal();
+    }
+
+    public static void printpayal(){
+        System.out.println("Hello Payal..!");
+    }
+}
