5c97677f84cfd22daa6dc4db05c69aa5d63075c9
Harsh Kumar <harsh.kumar9118b@gmail.com>
2026-01-06 22:52:23 +0530

    updated the BinaryToDecimal program to handle 0 and negative cases

--- Functions_and_Methods/BinaryToDecimal.java
+++ Functions_and_Methods/BinaryToDecimal.java
@@ -1,23 +1,38 @@
 public class BinaryToDecimal {
+
     public static void main(String[] args) {
-        // Convert binary number to decimal
         BinToDec(1010);
+        BinToDec(0);
+        BinToDec(-101);
     }
 
     // Converts a binary number to its decimal value
     public static void BinToDec(int n) {
+
+        // Handle zero explicitly
+        if (n == 0) {
+            System.out.println("The decimal number of 0 is 0");
+            return;
+        }
+
+        boolean isNegative = n < 0;
+        n = Math.abs(n);
 
         int mynum = n;  // Store original binary number
         int pow = 0;    // Power of 2
         int dec = 0;    // Decimal result
 
         while (n > 0) {
-            int lastdigit = n % 10;                       // Get last binary digit
+            int lastdigit = n % 10;
             dec += lastdigit * (int) Math.pow(2, pow);
             pow++;
-            n /= 10;                                      // Remove last digit
+            n /= 10;
         }
 
-        System.out.println("The decimal number of " + mynum + " is " + dec);
+        if (isNegative) {
+            System.out.println("The decimal number of -" + mynum + " is -" + dec);
+        } else {
+            System.out.println("The decimal number of " + mynum + " is " + dec);
+        }
     }
 }
